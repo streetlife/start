534c0ec0a1635db5ffa50c0166409b73a7aac7ee
Demola O <demolaoladipo@gmail.com>
2023-02-27 15:48:36 +0100

    1

--- refer.php
+++ refer.php
@@ -2,11 +2,17 @@
 $link = $_GET['link'];
 
 $log_file = 'hits.txt';
+
 $logs = json_decode(file_get_contents($log_file), true);
 
-$hit_count = $logs[$link];
+$hit_count = 0;
+if (isset($logs[$link])) {
+    $hit_count = $logs[$link];
+}
 
 $logs[$link] = $hit_count + 1;
+
+// arsort($logs);
 
 file_put_contents($log_file, json_encode($logs));
 
