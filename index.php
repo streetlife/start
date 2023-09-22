07d3297efadccb7cc8e2fd528b10904204d8690e
Demola O <demolaoladipo@gmail.com>
2023-09-22 18:18:21 +0100

    Latest update 20230922

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php 
 ini_set('display_errors', 0);
+$settings = [
+	'show_icon'=>true,
+	'log_clicks'=>false,
+	'refresh_rate'=>600,
+	'show_count'=>false,
+];
+
+
 $projects_folder = '..';
 $dirs = array_filter(glob($projects_folder . '/*'), 'is_dir');
 foreach ($dirs as $value) {
@@ -7,159 +15,80 @@ foreach ($dirs as $value) {
 	$project_links['https://'.$value.'.test'] = $value;
 }
 
-$log_file = 'hits.log';
+$log_file = 'data/hits.json';
+$todo_file = 'data/todo.json';
+$links_file = 'data/links.json';
+
+$links = json_decode(file_get_contents($links_file), true);
 $logs = json_decode(file_get_contents($log_file), true);
+$todos = json_decode(file_get_contents($todo_file), true);
 
-$settings = [
-	'show_icon'=>true,
-	'log_clicks'=>false,
-	'refresh_rate'=>20,
-];
+// Add a new todo
+if (isset($_POST['action']) && $_POST['action'] == 'add_todo') {
+	$todos = json_decode(file_get_contents($todo_file), true);
+	
+	if (empty($todos)) {
+		$todos = array();
+	}
+	$last_todo = end($todos);
+	$todo['id'] = $last_todo['id'] + 1;
+	$todo['text'] = $_POST['todo'];
+	$todo['done'] = false;
 
-$system_links = [
-	'http://localhost/phpmyadmin'=>'phpmyadmin',
-	'http://esquire-xps15:8989'=>'sonarr',
-	'http://esquire-xps15:8097'=>'jellyfin',
-	'https://web.whatsapp.com'=>'whatsapp',
-	'https://web.telegram.org'=>'telegram',
-	// 'http://esquire-xps15:9117'=>'jackett',
-	// 'http://esquire-xps15:7878'=>'radarr',
-];
+	$todos[] = $todo;
+	// array_push($todos, $_POST['todo']);
+	file_put_contents($todo_file, json_encode($todos, true));
+	header('Location: index.php');
+}
 
-$reading_links = [
-	'https://feedly.com'=>'feedly',
-	'https://reddit.com'=>'reddit',
-	'https://twitter.com'=>'twitter',
-	'https://nairaland.com'=>'nairaland',
-];
+// Delete a todo
+if (isset($_GET['action'])) {
+	if ($_GET['action'] == 'delete_todo') {
+		$todos = json_decode(file_get_contents($todo_file), true);
+		foreach ($todos as $key=>$todo) {
+			if ($todo['id']==$_GET['id']) {
+				$todos[$key]['done'] = true;
+			}
+		}
+		file_put_contents($todo_file, json_encode($todos, true));
+		header('Location: index.php');
+	}
+}
 
-$work_links = [
-	'https://smartwork.ng'=>'smartwork',
-	'https://mail.google.com'=>'gmail',
-	// 'https://goodday.work'=>'goodday',
-	// 'https://github.com'=>'github',
-	// 'https://keep.google.com'=>'keep',
-	// 'https://drive.google.com'=>'drive',
-	'https://pmi.org'=>'pmi',
-	'https://projectmanagementdocs.com'=>'project mgt docs',
-	'https://www.projectmanagement.com'=>'project mgt',
-	'https://www.stakeholdermap.com/project-templates/project-management-templates.html'=>'project mgt templates',
-	'https://outlook.live.com'=>'outlook',
-];
 
-$media_links = [
-	'https://youtube.com'=>'youtube',
-	// 'https://tiktok.com'=>'tiktok',
-	// 'https://photos.google.com'=>'photos',
-	// 'https://instagram.com'=>'instagram',
-	'https://www.wcofun.net'=>'wcofun',
-	'https://trakt.tv'=>'trakt',
-	'https://music.youtube.com'=>'music - youtube',
-	'https://spotify.com'=>'spotify',
-];
+// Generate a random color
+function rand_color() {
+	return sprintf('#%06X', mt_rand(0, 0xFFFFFF));
+}
 
-$games_links = [
-	'https://crazygames.com'=>'crazy games',
-	'https://epicgames.com'=>'epic games',
-	'https://gog.com'=>'gog',
-	'https://playclassic.games'=>'play classic games',
-];
-
-$sports_links = [
-	'https://livescore.com'=>'livescores',
-	'https://www.arsenal.com'=>'arsenal',
-	'https://premierleague.com'=>'premier league',
-	'https://kikgoal.com'=>'kik goal',
-];
-
-$learning_links = [
-	'https://exercism.org/dashboard'=>'exercism',
-	'https://campus.college.ch'=>'robert kennedy',
-	'https://coursera.org'=>'coursera',
-	'https://udemy.com'=>'udemy',
-	'https://freecodecamp.org'=>'freecodecamp',
-	'https://www.mygreatlearning.com'=>'great learning'
-];
-
-$utilities_links = [
-	'https://123apps.com'=>'123apps',
-	'https://sejda.com/pdf-editor'=>'sejda-pdf',
-	'https://alternativeto.net'=>'alternativeto',
-	'https://convertio.co'=>'convertio',
-];
-
-$warez_links = [
-	'https://wplocker.com'=>'wplocker',
-	'https://torrentgalaxy.to'=>'torrent galaxy',
-	'https://codelist.cc'=>'codelist',
-	'https://themelock.com'=>'themelock',
-    'https://steamrip.com'=>'steamrip',
-	'https://downloadly.ir'=>'downloadly',
-    'https://bitdownload.ir'=>'bitdownload',
-	'https://www.seriezloaded.com.ng/movies/nollywood-movies/'=>'nollywood movies',
-];
-
-$ai_links = [
-    'https://chat.openai.com'=>'chatGPT',
-    'https://lexica.art/aperture'=>'lexica',
-    'https://labs.openai.com'=>'dall-e',
-	'https://bing.com/new'=>'bing ai',
-	'https://bard.google.com'=>'bard',
-	'https://www.futurepedia.io/'=>'futurepedia',
-	'https://agentgpt.reworkd.ai/'=>'agent gpt',
-];
-
-$graphics_links = [
-	'https://themeforest.net'=>'themeforest',
-	'https://codecanyon.net'=>'codecanyon',
-	'https://canva.com'=>'canva',
-    'https://storyset.com'=>'story set',
-	'https://slidesgo.com'=>'slides go',
-	'https://slidescarnival.com'=>'slides carnival',
-	'https://wepik.com'=>'wepik',
-	'https://www.videvo.net/'=>'videvo',
-	'https://mixkit.co/'=>'mixkit',
-];
-
-$hosting_links = [
-	'https://whogohost.com'=>'whogohost',
-	'https://hostforweb.com'=>'hostforweb',
-	'https://websguy.com:2083'=>'websguy',
-	'https://ionos.com'=>'ionos',
-    'https://iwebfusion.net'=>'iwebfusion',
-];
-
-$clients_links = [
-	'https://portal.inceltourism.com'=>'incel portal',
-	'https://zerostore.com.ng'=>'zero store',
-	'https://lagosstate.gov.ng'=>'lagos state',
-	'https://webz.com.ng/awd/' => 'awd',
-	'https://webz.com.ng/twaccg/' => 'twaccg',
-];
-
-// $log_file = 'hits.txt';
-// $logs = json_decode(file_get_contents($log_file), true);
-
+/**
+ * Renders a list of links in a card format.
+ *
+ * @param array $links An array of links to be displayed.
+ * @param string $title The title of the card.
+ * @throws None
+ * @return void
+ */
 function show_links($links, $title) {
 	global $logs;
 	global $settings;
 
+	$border_color = rand_color();
+
 	natcasesort($links);
 	echo '
-	<div class="card mb-2 bg-transparent border-0 border-left-2">
+	<div class="card mb-2 bg-transparent border-top-2" style="border-top-color: '.$border_color.'">
 		<div class="card-body p-0 bg-transparent border-0">
 			<div class="card-header bg-transparent"><span class="title">'.$title.'</span></div>
 			<ul class="list-group list-group-flush border-0">';
 	foreach ($links as $key=>$value) {
 		
-		// $hit_count = intval($logs[$key]);
+		$hit_count = intval($logs[$key]);
 
-		// $name_display = $value;
-		// if ($hit_count > 0) {
-		// 	$name_display .= ' - '.$hit_count;
-		// }
-		
 		$name_display = $value; // . ' - ' . $hit_count;
+		if ($settings['show_count']) {
+			$name_display .= ' <span style="color:gray">:'.$hit_count . '</span>';
+		}
 		$value = strtolower(str_replace(' ','',$value));
 		$local_name_offline = 'img/icon-local.png';
 		
@@ -170,22 +99,20 @@ function show_links($links, $title) {
 			echo '<a href="' . $key . '" rel="noopener noreferrer" style="display:block">';
 		}
 		if ($settings['show_icon']) {
-			$local_name = $local_name_offline;
-			if (strpos($key,'test')==0) {
-				$local_name = 'img/icons/'.$value.'.png';
+			$local_name = 'img/icons/'.$value.'.png';
 
-				if ((isset($_GET['refresh_icons'])) || (!file_exists($local_name))) {
-					get_site_icon($local_name, $key);
-				}
+			if (!file_exists($local_name)) {
+				file_put_contents($local_name, file_get_contents('https://www.google.com/s2/favicons?domain='.$key.'&sz=256'));
+				// sleep(1);
+			}
 
-				if (filesize($local_name) == 0) {
-					$local_name = $local_name_offline;
-				}
-			}	
-			echo '<img src="'.$local_name.'" class="icon" /> ';
+			if (filesize($local_name) == 0) {
+				copy($local_name_offline, $local_name);
+			}
+			echo '<img src="'.$local_name.'" class="icon" style="clear:both" /> ';
 		}
-
-		echo '<span>' . $name_display. '</span></a>';
+		echo '<span class="icon-name">' . $name_display. '</span>';
+		echo '</a>';
 		echo '</li>';
 	}
 	echo '
@@ -194,10 +121,6 @@ function show_links($links, $title) {
 	</div>';
 }
 
-function get_site_icon($local_name, $key) {
-	file_put_contents($local_name, file_get_contents('https://www.google.com/s2/favicons?domain='.$key.'&sz=256'));
-
-}
 
 // function to randomly select a picture from a folder
 function display_random_wallpaper() {
@@ -206,7 +129,6 @@ function display_random_wallpaper() {
 
 	// Get all image files from the folder
 	$imageFiles = glob($imageFolder . '*.{jpg,jpeg,png}', GLOB_BRACE);
-	// die(print_r($imageFiles));
 
 	// Select a random image from the array
 	$randomImage = $imageFiles[array_rand($imageFiles)];
@@ -214,6 +136,25 @@ function display_random_wallpaper() {
 	// Display the random image on the webpage
 	return '<img src="' . $randomImage . '"  width="100%" class="img">';
 }
+
+// function to load a list of todos from a json file
+function load_todo() {
+	global $todos;
+
+	// Render the list
+	$result = '<ul class="list-group list-group-flush border-0 mb-2 bg-black">';
+	foreach ($todos as $todo) {
+		if ($todo['done']!=true) {
+			// $result .= '<li class="list-group-item text-white"><a href="index.php?action=delete_todo&id=' . $todo['id'] . '" class="text-white">' . $todo['text'] . '</a></li>';
+
+			$result .= '<a href="index.php?action=delete_todo&id=' . $todo['id'] . '" class="list-group-item list-group-item-action bg-transparent">' . $todo['text'] . '</a>';
+		}
+	}
+	$result .= '</ul>';
+	return $result;
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -225,60 +166,76 @@ function display_random_wallpaper() {
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta http-equiv="refresh" content="<?php echo $settings['refresh_rate']; ?>" />
 	</head>
-	<body class="bg-black bg-image">
+	<body class="bg-black">
 	
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-lg-4">
+				<div class="col-lg-2">
 					<div class="card p-0 m-0">
 						<div class="card-body p-0 m-0">
 							<?php echo display_random_wallpaper(); ?>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-8">
-					<div class="row">
-						
-					</div>
-					
+				<div class="col-lg-8">					
 					<div class="row">
 						<div class="col-lg-2 col-md-3 col-sm-4 p-0">
 							<?php 
 								show_links($project_links, 'projects'); 
-								show_links($system_links, 'system'); 
+								show_links($links['system'], 'system'); 
+								show_links($links['clients'], 'clients');
 							?>
 						</div>
 						<div class="col-lg-2 col-md-3 col-sm-4 p-0 border-left">
 							<?php 
-								show_links($work_links, 'work'); 
-								show_links($hosting_links, 'hosting');
+								show_links($links['work'], 'work'); 
+								show_links($links['projectmgt'], 'project mgt');
+								show_links($links['hosting'], 'hosting');
 							?>
 						</div>
 						<div class="col-lg-2 col-md-3 col-sm-4 p-0">
 							<?php 
-								show_links($clients_links, 'clients');
-								show_links($reading_links, 'reading');
-								show_links($ai_links, 'ai');
+								show_links($links['reading'], 'reading');
+								show_links($links['ai'], 'ai');
 							?>	
 						</div>
 						<div class="col-lg-2 col-md-3 col-sm-4 p-0">
 							<?php  
-								show_links($media_links, 'media'); 
-								show_links($graphics_links, 'graphics');
+								show_links($links['media'], 'media'); 
+								show_links($links['graphics'], 'graphics');
 							?>
 						</div>
 						<div class="col-lg-2 col-md-3 col-sm-4 p-0">
 							<?php 
-								show_links($learning_links, 'learning');
-								show_links($games_links, 'games');
-								show_links($sports_links, 'sports');
+								show_links($links['learning'], 'learning');
+								show_links($links['games'], 'games');
+								show_links($links['sports'], 'sports');
 							?>
 						</div>
 						<div class="col-lg-2 col-md-3 col-sm-4 p-0">
 							<?php 
-								show_links($utilities_links, 'utilities'); 
-								show_links($warez_links, 'warez'); 
+								show_links($links['utilities'], 'utilities'); 
+								show_links($links['warez'], 'warez'); 
 							?>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2">
+					<div class="card bg-black">
+						<div class="card-body">
+							<?php echo load_todo(); ?>
+						</div>
+					</div>
+					<div class="card bg-black">
+						<div class="card-body">
+							<form action="index.php" method="post" class="form">
+								<div class="form row align-items-center p-1">
+									<div class="col-8"><input type="text" name="todo" class="form-control form-control-sm m-0"></div>
+									<div class="col-2"><button type="submit" value="Add" class="btn btn-primary m-0">Add</button></div>
+									<input type="hidden" name="action" value="add_todo">
+								</div>
+															
+							</form>
 						</div>
 					</div>
 				</div>
